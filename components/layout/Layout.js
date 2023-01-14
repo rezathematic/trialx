5aedfe8e7d1c5900941cfe2d3bf4d27bb3311402
Reza <r.bahar91@gmail.com>
2023-01-14 14:42:27 +0530

    add: reusable layout with navbar

---
+++ components/layout/Layout.js
@@ -0,0 +1,11 @@
+// Component imports
+import Navbar from "@/components/layout/Navbar";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main className="ml-20 w-full">{children}</main>
+    </>
+  );
+}
