a4c04feb9f3874558aa723b20f2c1a195d32eb45
Reza <r.bahar91@gmail.com>
2023-01-19 23:40:17 +0530

    add: added patient card to a view

---
+++ views/patients/CardPatient.js
@@ -0,0 +1,9 @@
+import PatientCard from "@/components/patients/PatientCard";
+
+export default function CardPatient({ patient }) {
+  return (
+    <>
+      <PatientCard patient={patient} />
+    </>
+  );
+}
