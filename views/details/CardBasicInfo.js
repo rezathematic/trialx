55f726e02a5e3c66f5ecb00fa7ecdf1dfdd40fd8
Reza <r.bahar91@gmail.com>
2023-01-20 13:22:20 +0530

    add: patient basic info card view

---
+++ views/details/CardBasicInfo.js
@@ -0,0 +1,18 @@
+// Component Imports
+import PatientAvatar from "@/components/patients/basicInfo/PatientAvatar";
+import PatientName from "@/components/patients/basicInfo/PatientName";
+import PatientStatus from "@/components/patients/basicInfo/PatientStatus";
+import PatientTable from "@/components/patients/basicInfo/PatientTable";
+
+export default function CardBasicInfo({ patient }) {
+  const { name, image, status } = patient;
+
+  return (
+    <div className="flex flex-col rounded-md border border-slate-100 bg-white p-4 shadow-lg shadow-slate-200">
+      <PatientAvatar src={image} name={name} />
+      <PatientName name={name} />
+      <PatientStatus status={status} />
+      <PatientTable {...patient} />
+    </div>
+  );
+}
