9710f058b4fabb379bae0b67a4b5561289773021
Reza <r.bahar91@gmail.com>
2023-01-20 13:20:56 +0530

    add: patient basic info avatar init

---
+++ components/patients/basicInfo/PatientAvatar.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function PatientAvatar({ src, name }) {
+  return (
+    <div className="relative h-full w-full">
+      <Image
+        src={src ? src : "/assets/images/userp.png"}
+        alt={name}
+        width={100}
+        height={100}
+        className="h-full w-full rounded-sm object-cover shadow-sm"
+      />
+    </div>
+  );
+}
