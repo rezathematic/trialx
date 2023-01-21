a5ffdb985cecf29d006e0f52ebf5b266aeae3649
Reza <r.bahar91@gmail.com>
2023-01-21 13:36:45 +0530

    merged with component under /allergy folder

--- components/patients/details/PatientAllergy.js
+++ components/patients/details/PatientAllergy.js
@@ -1,15 +1,17 @@
 export default function PatientAllergy({ patient }) {
+  const { allergies } = patient;
+
   return (
     <div>
       <h2 className="text-xl font-medium tracking-wide text-slate-500">
         Allergies
       </h2>
-      <div className="mt-4 flex flex-col text-slate-700">
-        <table className="border-separate border-spacing-y-3">
-          <tbody>
-            {patient.allergies.map((allergy, index) => (
+      <div className="flex flex-col">
+        <table className="border-separate border-spacing-y-4">
+          <tbody className="text-sm text-slate-500">
+            {allergies.map((allergy, index) => (
               <tr key={index}>
-                <td>{allergy.drug}</td>
+                <td className="font-medium text-slate-600">{allergy.drug}</td>
                 <td>{allergy.severity}</td>
               </tr>
             ))}
