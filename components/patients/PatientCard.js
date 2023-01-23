4b5ef3a26096fffc6ebd749a094da3718289bcaf
Reza <r.bahar91@gmail.com>
2023-01-24 00:37:25 +0530

    integrated with api and linked to patient's page

--- components/patients/PatientCard.js
+++ components/patients/PatientCard.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 // compponent imports
 import PatientCardAvatar from "@/components/patients/PatientCardAvatar";
 import PatientCardName from "@/components/patients/PatientCardName";
@@ -6,19 +8,25 @@ import PatientCardStatus from "@/components/patients/PatientCardStatus";
 import PatientCardAddInfo from "@/components/patients/PatientCardAddInfo";
 
 export default function PatientCard({ patient }) {
-  const { id, name, image, status, sex, age, phone } = patient;
+  const id = patient.uid;
+  const {
+    basicInfo: { firstName, lastName, image, sex, age, phone, status },
+  } = patient;
+  const name = `${firstName} ${lastName}`;
 
   return (
-    <div className="flex aspect-[16/9] flex-col rounded-md border border-slate-100 bg-white p-4 shadow-lg shadow-slate-200 xl:col-span-2">
-      <div className="flex">
-        <PatientCardAvatar src={image} name={name} />
-        <div>
-          <PatientCardName name={name} />
-          <PatientCardId id={id} />
-          <PatientCardStatus status={status} />
+    <div className="flex aspect-[16/9] flex-col rounded-md border border-slate-100 bg-white p-4 shadow-lg shadow-slate-200 transition duration-300 hover:scale-105 xl:col-span-2">
+      <Link href={`/patients/${id}`}>
+        <div className="flex">
+          <PatientCardAvatar src={image} name={name} />
+          <div>
+            <PatientCardName name={name} />
+            <PatientCardId id={id} />
+            <PatientCardStatus status={status} />
+          </div>
         </div>
-      </div>
-      <PatientCardAddInfo sex={sex} age={age} phone={phone} />
+        <PatientCardAddInfo sex={sex} age={age} phone={phone} />
+      </Link>
     </div>
   );
 }
