5d48a8e57a806522096d400bc9572659259a5747
Reza <r.bahar91@gmail.com>
2023-01-25 19:26:24 +0530

    added gender info card view

---
+++ views/overview/CardGenderInfo.js
@@ -0,0 +1,9 @@
+import ChartGenderInfo from "@/components/overview/ChartGenderInfo";
+
+export default function CardGenderInfo({ genderData }) {
+  return (
+    <>
+      <ChartGenderInfo data={genderData} />
+    </>
+  );
+}
