fadf308ed91a4b6ae01c104d59c3d49fb7de188d
Reza <r.bahar91@gmail.com>
2023-01-25 19:26:54 +0530

    imported new views and API integration

--- views/overview/Overview.js
+++ views/overview/Overview.js
@@ -5,16 +5,24 @@ import Grid from "@/components/utils/Grid";
 // View imports
 import CardTrialInfo from "@/views/overview/CardTrialInfo";
 import CardSampleInfo from "@/views/overview/CardSampleInfo";
+import CardGenderInfo from "@/views/overview/CardGenderInfo";
 import CardAdr from "@/views/overview/CardAdr";
 import CardAntibody from "@/views/overview/CardAntibody";
 
-export default function Overview() {
+// Lib imports
+import { genderDateCount, genderCount } from "@/lib/utils";
+
+export default function Overview({ trial }) {
+  const genderCountData = genderCount(trial);
+  const genderDateData = genderDateCount(trial);
+
   return (
     <>
       <PageTitle>Overview</PageTitle>
       <Grid>
         <CardTrialInfo />
-        <CardSampleInfo />
+        <CardSampleInfo sampleData={genderCountData} />
+        <CardGenderInfo genderData={genderDateData} />
         <CardAdr />
         <CardAntibody />
       </Grid>
