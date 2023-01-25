d23b78b95378c8d16e4390107b4c03c893de2384
Reza <r.bahar91@gmail.com>
2023-01-25 19:26:34 +0530

    api integration

--- views/overview/CardSampleInfo.js
+++ views/overview/CardSampleInfo.js
@@ -1,9 +1,9 @@
 import ChartSampleInfo from "@/components/overview/ChartSampleInfo";
 
-export default function CardSampleInfo() {
+export default function CardSampleInfo({ sampleData }) {
   return (
     <>
-      <ChartSampleInfo />
+      <ChartSampleInfo data={sampleData} />
     </>
   );
 }
