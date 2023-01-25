8f4eb8ab01304fe6fbedfe11259aa179e656cd6d
Reza <r.bahar91@gmail.com>
2023-01-25 19:24:21 +0530

    api integration

--- components/overview/ChartSampleInfo.js
+++ components/overview/ChartSampleInfo.js
@@ -9,47 +9,20 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    date: "Jan, 14",
-    male: 6,
-    female: 5,
-  },
-  {
-    date: "Jan, 15",
-    male: 6,
-    female: 5,
-  },
-  {
-    date: "Jan, 16",
-    male: 6,
-    female: 5,
-  },
-  {
-    date: "Jan, 17",
-    male: 6,
-    female: 6,
-  },
-  {
-    date: "Jan, 18",
-    male: 6,
-    female: 6,
-  },
-];
-
-export default function ChartSampleInfo() {
+export default function ChartSampleInfo({ data }) {
+  const { Male, Female } = data;
   return (
-    <div className="col-span-12 lg:col-span-6">
+    <div className="col-span-12 lg:col-span-3">
       <div className="flex h-full w-full flex-col rounded-lg border border-slate-100 bg-white py-8 px-4 shadow-lg">
         <h2 className="text-xl font-semibold tracking-wide text-slate-700">
-          Sample Details
+          Sample&apos;s Gender Distribution
         </h2>
         <div className="mt-8 h-full w-full">
           <ResponsiveContainer width="100%" height="100%">
             <BarChart
               width={500}
               height={300}
-              data={data}
+              data={[{ Male: Male, Female: Female }]}
               margin={{
                 top: 5,
                 right: 30,
@@ -62,8 +35,8 @@ export default function ChartSampleInfo() {
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="male" fill="#8884d8" />
-              <Bar dataKey="female" fill="#82ca9d" />
+              <Bar dataKey="Male" stackId="a" fill="#8884d8" />
+              <Bar dataKey="Female" stackId="a" fill="#82ca9d" />
             </BarChart>
           </ResponsiveContainer>
         </div>
