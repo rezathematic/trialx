90b944f5af856d94bba7a5360800574e85a5e711
Reza <r.bahar91@gmail.com>
2023-01-25 19:24:03 +0530

    add: gender count based on added date component

---
+++ components/overview/ChartGenderInfo.js
@@ -0,0 +1,45 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function ChartGenderInfo({ data }) {
+  return (
+    <div className="col-span-12 lg:col-span-3">
+      <div className="flex h-full w-full flex-col rounded-lg border border-slate-100 bg-white py-8 px-4 shadow-lg">
+        <h2 className="text-xl font-semibold tracking-wide text-slate-700">
+          Gender Info Based on Added Date
+        </h2>
+        <div className="mt-8 h-full w-full">
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="date" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="Male" stackId="a" fill="#8884d8" />
+              <Bar dataKey="Female" stackId="a" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
