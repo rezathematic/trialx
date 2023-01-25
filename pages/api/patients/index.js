2dde635eafea0dfb00374d0b3bb93790d7e0118f
Reza <r.bahar91@gmail.com>
2023-01-25 16:41:08 +0530

    refactored create patient

--- pages/api/patients/index.js
+++ pages/api/patients/index.js
@@ -1,30 +1,12 @@
-import prisma from "@/lib/prisma";
+import { createNewPatient } from "@/lib/api";
 
 export default async function handler(req, res) {
   // if method is POST, create new patient
   if (req.method === "POST") {
-    const newPatient = await prisma.patient.create({
-      data: {
-        basicInfo: {
-          create: {
-            ...req.body,
-          },
-        },
-        adr: {
-          create: {
-            bronchitis: false,
-            diarrhea: false,
-            pharyngitis: false,
-            pruritus: false,
-            rash: false,
-            rhinitis: false,
-            urticaria: false,
-          },
-        },
-      },
-    });
+    const newPatient = createNewPatient(req.body);
     res.json(newPatient);
   }
+
   // if method is not allowed
   else {
     res.status(405).json({ message: "Method not allowed" });
