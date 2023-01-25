d5bf1e91793cbf4dd4d1b59fd23fe2a1a0390db7
Reza <r.bahar91@gmail.com>
2023-01-25 19:25:59 +0530

    API integration and server side data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 
+// Lib imports
+import prisma from "@/lib/prisma";
+import { serializeJSON } from "@/lib/utils";
+
 // View imports
 import Overview from "@/views/overview/Overview";
 
-export default function Home() {
+export default function Home({ trial }) {
   return (
     <>
       <Head>
@@ -14,8 +18,22 @@ export default function Home() {
       </Head>
 
       <div className="w-full px-8">
-        <Overview />
+        <Overview trial={trial} />
       </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const patients = await prisma.patient.findMany({
+    include: {
+      basicInfo: true,
+    },
+  });
+
+  return {
+    props: {
+      trial: serializeJSON(patients),
+    },
+  };
 }
