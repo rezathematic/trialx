7974c9a2930598508ec16218a11e98d526bd6bea
Reza <r.bahar91@gmail.com>
2023-01-31 19:35:45 +0530

    adjusted colors based on health status

--- components/patients/PatientCardStatus.js
+++ components/patients/PatientCardStatus.js
@@ -1,16 +1,42 @@
+import cn from "classnames";
+
 export default function PatientCardStatus({ status }) {
   return (
     <div className="mt-2 flex items-center">
-      <div className="flex h-4 w-4 items-center justify-center rounded-full bg-green-100">
+      <div
+        className={cn("flex h-4 w-4 items-center justify-center rounded-full", {
+          "bg-green-100": status === "Healthy",
+          "bg-blue-100": status === "Mild",
+          "bg-yellow-100": status === "Moderate",
+          "bg-orange-100": status === "Severe",
+          "bg-red-100": status === "Critical",
+        })}
+      >
         <svg
-          className="h-2 w-2 text-green-500"
+          className={cn("h-2 w-2", {
+            "text-green-500": status === "Healthy",
+            "text-blue-500": status === "Mild",
+            "text-yellow-500": status === "Moderate",
+            "text-orange-500": status === "Severe",
+            "text-red-500": status === "Critical",
+          })}
           fill="currentColor"
           viewBox="0 0 8 8"
         >
           <circle cx="4" cy="4" r="3" />
         </svg>
       </div>
-      <p className="ml-2 text-sm text-green-500">{status}</p>
+      <p
+        className={cn("ml-2 text-sm font-medium tracking-wide", {
+          "text-green-500": status === "Healthy",
+          "text-blue-500": status === "Mild",
+          "text-yellow-500": status === "Moderate",
+          "text-orange-500": status === "Severe",
+          "text-red-500": status === "Critical",
+        })}
+      >
+        {status}
+      </p>
     </div>
   );
 }
