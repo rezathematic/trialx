86171803f6d89ebf63a1cd887eb90afb933f720c
Reza <r.bahar91@gmail.com>
2023-01-31 17:54:56 +0530

    Added NextAuth session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Router from "next/router";
 import NProgress from "nprogress";
 import { useState } from "react";
+import { SessionProvider } from "next-auth/react";
 
 // Style imports
 import "@/styles/globals.css";
@@ -10,7 +11,10 @@ import "nprogress/nprogress.css";
 import Layout from "@/components/layout/Layout";
 import Spinner from "@/components/utils/Spinner";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   const [loading, setLoading] = useState(false);
 
   NProgress.configure({ showSpinner: false });
@@ -28,13 +32,15 @@ export default function App({ Component, pageProps }) {
     NProgress.done();
   });
   return (
-    <Layout>
-      {loading && (
-        <div className="fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-white bg-opacity-80">
-          <Spinner />
-        </div>
-      )}
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        {loading && (
+          <div className="fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-white bg-opacity-80">
+            <Spinner />
+          </div>
+        )}
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
