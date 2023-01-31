53d72c67485982b5bb35a8cfdb1d15b591debdd4
Reza <r.bahar91@gmail.com>
2023-01-31 18:41:28 +0530

    added google user content to hostname

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
