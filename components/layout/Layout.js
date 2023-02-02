1b6f098a9dcb549506747ce13352238d288eb1d8
Reza <r.bahar91@gmail.com>
2023-02-03 01:10:52 +0530

    minor changes to sign in page

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -21,13 +21,23 @@ export default function Layout({ children }) {
       <>
         <main>
           <div className="flex h-screen flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold">Please sign in</h1>
-            <button
-              className="mt-4 rounded-md bg-blue-600 px-4 py-2 text-white"
-              onClick={() => signIn()}
-            >
-              Sign In
-            </button>
+            <div className="border border-slate-100 p-12 text-center shadow-lg">
+              <h1 className="mb-4 text-3xl font-semibold text-slate-700">
+                Welcome to TRIAL X
+              </h1>
+              <h2 className="mb-4 text-slate-700">
+                Please sign in to continue
+              </h2>
+              <button
+                className="mt-4 rounded-md bg-blue-600 px-4 py-2 text-white"
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
+                Sign In
+              </button>
+            </div>
           </div>
         </main>
       </>
